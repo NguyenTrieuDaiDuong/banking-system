6dce27a04f552f51756ff4fbb1cd8c56d3fab1d3
NguyenTrieuDaiDuong <duongnguyen22394@example.com>
2025-10-23 16:05:36 +0700

    Add : to view account details by account number

--- src/main/java/com/example/demo/controllers/api/AccountController.java
+++ src/main/java/com/example/demo/controllers/api/AccountController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,13 @@ public class AccountController {
 		String username = authentication.getName();
 		List<AccountResponse> myAccount = accountService.getUserAccounts(username);
 		return ResponseEntity.ok(myAccount);
+	}
+
+	@GetMapping("/{accountNumber}")
+	public ResponseEntity<AccountResponse> getAccountDetails(@PathVariable("accountNumber") String accountNumber,
+			Authentication authentication) {
+		String username = authentication.getName();
+		AccountResponse response = accountService.getAccountDetails(accountNumber, username);
+		return ResponseEntity.ok(response);
 	}
 }
